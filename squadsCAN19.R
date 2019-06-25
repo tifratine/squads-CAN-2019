2259f3edf76bcfbb9f6e7701f28d3408c77862d6
tifratine <atifratine@episteme.ma>
2019-06-25 22:35:07 +0100

    Add files via upload

---
+++ squadsCAN19.R
@@ -0,0 +1,31 @@
+#packages
+library(rvest)
+setwd("C:\\Users\\TIFRATINE\\Desktop\\CAN 2019")
+
+library(tidyverse)
+
+#Scrapping
+
+
+countries<-c("Egypt","DR Congo", "Uganda", "Zimbabwe",
+             "Nigeria", "Guinea", "Madagascar", "Burundi",
+             "Senegal", "Algeria", "Kenya", "Tanzania",
+             "Morocco", "Ivory Coast", "South Africa", "Namibia",
+             "Tunisia", "Mali", "Mauritania", "Angola",
+             "Cameroon", "Ghana", "Benin", "Guinea-Bissau")
+
+url <- "https://en.wikipedia.org/wiki/2019_Africa_Cup_of_Nations_squads"
+teams<-data.frame()
+for (i in 1:length(countries)){
+  xpth <-paste('//*[@id="mw-content-text"]/div/table[',i,']',sep = "")
+  team <- url %>%
+    html() %>%
+    html_nodes(xpath=xpth) %>%
+    html_table()
+  team <- team[[1]]
+  team$team<-countries[i]
+  team <- team[, c("team", "No.", "Pos.", "Player", "Date of birth (age)", "Caps", "Club")]
+  teams<-rbind(teams, team)
+}
+
+write.csv(teams,"teams_CAN_2019.csv")
